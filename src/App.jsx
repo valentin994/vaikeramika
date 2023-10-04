4c78f643c42d60f80f8bab237e8107e91fb76954
valentin994 <valentin.vareskic@gmail.com>
2023-10-04 11:53:40 +0200

    added home component, basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Nav from "./components/Nav.jsx";
-import { BrowserRouter as Router } from "react-router-dom";
+import Home from "./components/Home.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <div className="h-screen bg-lightbg text-textcol font-sans dark:bg-textcol">
         <Nav />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
       </div>
     </Router>
   );
---
+++ src/components/Home.jsx
@@ -0,0 +1,19 @@
+function Home() {
+  return (
+    <div className="flex justify-center">
+      <div className="container">
+        <div className="flex">
+          <div className="w-1/3 h-80 bg-primary m-1 p-2">
+            <p>Više od 20 godina iskustva</p>
+          </div>
+          <div className="w-2/3 h-80 bg-primary m-1"></div>
+        </div>
+        <div className="flex">
+          <div className="w-2/3 h-80 bg-primary m-1"></div>
+          <div className="w-1/3 h-80 bg-primary m-1"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Home;
