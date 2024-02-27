8efb66d6fe607880e977537abd9a1d3c7d495472
valentin994 <valentin.vareskic@gmail.com>
2024-02-27 09:06:58 +0100

    fixed the page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,5 @@
 <script>
   import "tailwindcss/tailwind.css";
-
-  let isScrolled = false;
-  window.addEventListener('scroll', () => {
-      isScrolled = window.scrollY > 0;
-  });
 </script>
 
 <svelte:head>
@@ -19,7 +14,7 @@
   <input id="my-drawer-3" type="checkbox" class="drawer-toggle" /> 
   <div class="drawer-content flex flex-col">
     <!-- Navbar -->
-    <div class="{isScrolled ? 'shadow-xl' : 'shadow-none'} bg-base-100 ease-in duration-300 w-full navbar sticky top-0 z-10">
+    <div class="bg-base-100 ease-in duration-300 w-full navbar sticky top-0 z-10">
       <div class="flex-none lg:hidden">
         <label for="my-drawer-3" aria-label="open sidebar" class="btn btn-square btn-ghost">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
