1488d1005a7d5548a74ea069c7ef2c6c69eff969
Valentin Vareskic <valentin.vareskic@gmail.com>
2024-03-04 16:08:09 +0100

    added background to homepage, added active link tracking

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,85 +1,104 @@
-<!-- TW Elements is free under AGPL, with commercial license required for specific uses. See more details: https://tw-elements.com/license/ and contact us for queries at tailwind@mdbootstrap.com --> 
+<!-- TW Elements is free under AGPL, with commercial license required for specific uses. See more details: https://tw-elements.com/license/ and contact us for queries at tailwind@mdbootstrap.com -->
 <div class="container mx-auto px-5 py-2 lg:px-32 lg:pt-24">
-  <div class="-m-1 flex flex-wrap md:-m-2">
-    <div class="flex w-1/2 flex-wrap">
-      <div class="w-1/2 p-1 md:p-2">
-        <img
-          alt="gallery"
-          class="block h-full w-full rounded-lg object-cover object-center"
-          src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp" />
-      </div>
-      <div class="w-1/2 p-1 md:p-2">
-        <img
-          alt="gallery"
-          class="block h-full w-full rounded-lg object-cover object-center"
-          src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp" />
-      </div>
-      <div class="w-full p-1 md:p-2">
-        <img
-          alt="gallery"
-          class="block h-full w-full rounded-lg object-cover object-center"
-          src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp" />
-      </div>
-    </div>
-    <div class="flex w-1/2 flex-wrap">
-      <div class="w-full p-1 md:p-2">
-        <img
-          alt="gallery"
-          class="block h-full w-full rounded-lg object-cover object-center"
-          src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp" />
-      </div>
-      <div class="w-1/2 p-1 md:p-2">
-        <img
-          alt="gallery"
-          class="block h-full w-full rounded-lg object-cover object-center"
-          src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(75).webp" />
-      </div>
-      <div class="w-1/2 p-1 md:p-2">
-        <img
-          alt="gallery"
-          class="block h-full w-full rounded-lg object-cover object-center"
-          src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(77).webp" />
-      </div>
-    </div>
-  </div>
-  <div class="divider"></div>
-    <div class="w-full">
-      <h1 class="w-full text-3xl p-4 text-center">Recenzije</h1>
-    </div>
-  <div class="w-full flex justify-evenly py-4">
-    <div class="card w-96 glass">
-      <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="car!"/></figure>
-        <div class="card-body">
-        <h2 class="card-title">Life hack</h2>
-        <p>How to park your car at your garage?</p>
-        <div class="card-actions justify-end">
-          <button class="btn btn-primary">Learn now!</button>
-        </div>
-      </div>
-    </div>
+	<div class="-m-1 flex flex-wrap md:-m-2">
+		<div class="flex w-1/2 flex-wrap">
+			<div class="w-1/2 p-1 md:p-2">
+				<img
+					alt="gallery"
+					class="block h-full w-full rounded-lg object-cover object-center"
+					src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp"
+				/>
+			</div>
+			<div class="w-1/2 p-1 md:p-2">
+				<img
+					alt="gallery"
+					class="block h-full w-full rounded-lg object-cover object-center"
+					src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp"
+				/>
+			</div>
+			<div class="w-full p-1 md:p-2">
+				<img
+					alt="gallery"
+					class="block h-full w-full rounded-lg object-cover object-center"
+					src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+				/>
+			</div>
+		</div>
+		<div class="flex w-1/2 flex-wrap">
+			<div class="w-full p-1 md:p-2">
+				<img
+					alt="gallery"
+					class="block h-full w-full rounded-lg object-cover object-center"
+					src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp"
+				/>
+			</div>
+			<div class="w-1/2 p-1 md:p-2">
+				<img
+					alt="gallery"
+					class="block h-full w-full rounded-lg object-cover object-center"
+					src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(75).webp"
+				/>
+			</div>
+			<div class="w-1/2 p-1 md:p-2">
+				<img
+					alt="gallery"
+					class="block h-full w-full rounded-lg object-cover object-center"
+					src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(77).webp"
+				/>
+			</div>
+		</div>
+	</div>
+	<div class="divider"></div>
+	<div class="w-full">
+		<h1 class="w-full text-3xl p-4 text-center">Recenzije</h1>
+	</div>
+	<div class="w-full flex justify-evenly py-4">
+		<div class="card w-96 glass">
+			<figure>
+				<img
+					src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+					alt="car!"
+				/>
+			</figure>
+			<div class="card-body">
+				<h2 class="card-title">Life hack</h2>
+				<p>How to park your car at your garage?</p>
+				<div class="card-actions justify-end">
+					<button class="btn btn-primary">Learn now!</button>
+				</div>
+			</div>
+		</div>
 
-    <div class="card w-96 glass">
-      <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="car!"/></figure>
-      <div class="card-body">
-        <h2 class="card-title">Life hack</h2>
-        <p>How to park your car at your garage?</p>
-        <div class="card-actions justify-end">
-          <button class="btn btn-primary">Learn now!</button>
-        </div>
-      </div>
-    </div>
+		<div class="card w-96 glass">
+			<figure>
+				<img
+					src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+					alt="car!"
+				/>
+			</figure>
+			<div class="card-body">
+				<h2 class="card-title">Life hack</h2>
+				<p>How to park your car at your garage?</p>
+				<div class="card-actions justify-end">
+					<button class="btn btn-primary">Learn now!</button>
+				</div>
+			</div>
+		</div>
 
-    
-    <div class="card w-96 glass">
-      <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="car!"/></figure>
-      <div class="card-body">
-        <h2 class="card-title">Life hack</h2>
-        <p>How to park your car at your garage?</p>
-        <div class="card-actions justify-end">
-          <button class="btn btn-primary">Learn now!</button>
-        </div>
-      </div>
-    </div>
-
-  </div>
+		<div class="card w-96 glass">
+			<figure>
+				<img
+					src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+					alt="car!"
+				/>
+			</figure>
+			<div class="card-body">
+				<h2 class="card-title">Life hack</h2>
+				<p>How to park your car at your garage?</p>
+				<div class="card-actions justify-end">
+					<button class="btn btn-primary">Learn now!</button>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
-  daisyui: {
-        themes: [
-            "light", 
-            "dark",
-            {
-                mytheme: {
-                    "primary": "#FF5733",
-                    "secondary": "#171612",
-                    "base-100": "#FCFCFC",
-                },
-            }
-        ],
-    }
-}
-
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {}
+	},
+	plugins: [require('@tailwindcss/typography'), require('daisyui')],
+	daisyui: {
+		themes: [
+			'light',
+			'dark',
+			{
+				mytheme: {
+					primary: '#b89260',
+					secondary: '#171612',
+					'base-100': '#FCFCFC'
+				}
+			}
+		]
+	}
+};
