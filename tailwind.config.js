6bc08968cc37e82a4e3008b98cadc7721c515ed4
valentin994 <valentin.vareskic@gmail.com>
2024-03-21 21:18:03 +0100

    updated landing

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ const colors = require("tailwindcss/colors")
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+          colors: {
+                background: "#FDFDFD",
+                textcol: "#434343",
+                primary: "#E6AF2E"
+            }
+        }
 	},
 	plugins: [require('@tailwindcss/typography')],
 };
