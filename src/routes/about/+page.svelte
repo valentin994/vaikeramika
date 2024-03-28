159c48884db07000dde5a7b4d12e60edd38d59c7
valentin994 <valentin.vareskic@gmail.com>
2024-03-28 13:04:52 +0100

    updated about section

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,15 @@
-<div>
-    about
+<div class="flex flex-col justify-center">
+	<h1 class="p-4 text-xl text-center">Nudimo širok spektar usluga, od jednostavnog postavljanja podnih pločica do složenih zidnih obloga i dekoracija.</h1>
+    <div class="flex flex-col p-4 my-4 justify-center items-center bg-gray-300 bg-opacity-25">
+        <img alt="House" src="./house2.svg" class="w-16" />
+        <h2 class="text-gray-500 p-4">Odradeno vise od 1000 domova. </h2>
+    </div>
+    <div class="flex flex-col p-4 my-4 justify-center items-center bg-gray-300 bg-opacity-25">
+        <img alt="Ruler" src="./ruler3.svg" class="w-16" />
+        <h2 class="text-gray-500 p-4">Vise od 10000 m2</h2>
+    </div>
+    <div class="flex flex-col p-4 my-4 justify-center items-center bg-gray-300 bg-opacity-25">
+        <img alt="Box" src="./kutija.svg" class="w-16" />
+        <h2 class="text-gray-500 p-4">Tisuce zadovoljnih klijenata</h2>
+    </div>
 </div>
