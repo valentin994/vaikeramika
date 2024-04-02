b195f1054e4628916719ad172a1715ee9e9cf52b
Valentin Vareskic <valentin.vareskic@gmail.com>
2024-04-02 15:04:32 +0200

    added placeholder gallery

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -11,7 +11,7 @@
       <label class="block text-gray-700 text-sm font-bold mb-2" for="poruka">
         poruka
       </label>
-      <textarea class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline focus:outline-secondary" id="poruka"></textarea>
+      <textarea rows="4" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline focus:outline-secondary" id="poruka"></textarea>
     </div>
     <div class="flex items-center justify-between">
       <button class="bg-secondary text-background font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,104 +1,48 @@
-<!-- TW Elements is free under AGPL, with commercial license required for specific uses. See more details: https://tw-elements.com/license/ and contact us for queries at tailwind@mdbootstrap.com -->
-<div class="container mx-auto px-5 py-2 lg:px-32 lg:pt-24">
-	<div class="-m-1 flex flex-wrap md:-m-2">
-		<div class="flex w-1/2 flex-wrap">
-			<div class="w-1/2 p-1 md:p-2">
-				<img
-					alt="gallery"
-					class="block h-full w-full rounded-lg object-cover object-center"
-					src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp"
-				/>
-			</div>
-			<div class="w-1/2 p-1 md:p-2">
-				<img
-					alt="gallery"
-					class="block h-full w-full rounded-lg object-cover object-center"
-					src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp"
-				/>
-			</div>
-			<div class="w-full p-1 md:p-2">
-				<img
-					alt="gallery"
-					class="block h-full w-full rounded-lg object-cover object-center"
-					src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-				/>
-			</div>
-		</div>
-		<div class="flex w-1/2 flex-wrap">
-			<div class="w-full p-1 md:p-2">
-				<img
-					alt="gallery"
-					class="block h-full w-full rounded-lg object-cover object-center"
-					src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp"
-				/>
-			</div>
-			<div class="w-1/2 p-1 md:p-2">
-				<img
-					alt="gallery"
-					class="block h-full w-full rounded-lg object-cover object-center"
-					src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(75).webp"
-				/>
-			</div>
-			<div class="w-1/2 p-1 md:p-2">
-				<img
-					alt="gallery"
-					class="block h-full w-full rounded-lg object-cover object-center"
-					src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(77).webp"
-				/>
-			</div>
-		</div>
-	</div>
-	<div class="divider"></div>
-	<div class="w-full">
-		<h1 class="w-full text-3xl p-4 text-center">Recenzije</h1>
-	</div>
-	<div class="w-full flex justify-evenly py-4">
-		<div class="card w-96 glass">
-			<figure>
-				<img
-					src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-					alt="car!"
-				/>
-			</figure>
-			<div class="card-body">
-				<h2 class="card-title">Life hack</h2>
-				<p>How to park your car at your garage?</p>
-				<div class="card-actions justify-end">
-					<button class="btn btn-primary">Learn now!</button>
-				</div>
-			</div>
-		</div>
+<script>
+  let selected = "https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1470&amp;q=80";
+  let imageList = [
+    "https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1470&amp;q=80",
+    "https://images.unsplash.com/photo-1432462770865-65b70566d673?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1950&amp;q=80",
+    "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2940&amp;q=80",
+    "https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2762&amp;q=80",
+    "https://images.unsplash.com/photo-1682407186023-12c70a4a35e0?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2832&amp;q=80",
 
-		<div class="card w-96 glass">
-			<figure>
-				<img
-					src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-					alt="car!"
-				/>
-			</figure>
-			<div class="card-body">
-				<h2 class="card-title">Life hack</h2>
-				<p>How to park your car at your garage?</p>
-				<div class="card-actions justify-end">
-					<button class="btn btn-primary">Learn now!</button>
-				</div>
-			</div>
-		</div>
+    "https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1470&amp;q=80",
+    "https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1470&amp;q=80",
+    "https://images.unsplash.com/photo-1432462770865-65b70566d673?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1950&amp;q=80",
+    "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2940&amp;q=80",
+    "https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2762&amp;q=80",
+    "https://images.unsplash.com/photo-1682407186023-12c70a4a35e0?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2832&amp;q=80",
+    "https://images.unsplash.com/photo-1432462770865-65b70566d673?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1950&amp;q=80",
+    "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2940&amp;q=80",
+    "https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2762&amp;q=80",
+    "https://images.unsplash.com/photo-1682407186023-12c70a4a35e0?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2832&amp;q=80",
+  ];
+</script>
+<h1 class="text-xl text-secondary font-semibold text-center p-4">Nasi radovi</h1>
 
-		<div class="card w-96 glass">
-			<figure>
-				<img
-					src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-					alt="car!"
-				/>
-			</figure>
-			<div class="card-body">
-				<h2 class="card-title">Life hack</h2>
-				<p>How to park your car at your garage?</p>
-				<div class="card-actions justify-end">
-					<button class="btn btn-primary">Learn now!</button>
-				</div>
-			</div>
-		</div>
-	</div>
+
+<div class="grid gap-4 p-4 md:px-24">
+  <div>
+    <img class="h-auto w-full max-w-full shadow-md border border-gray-300 rounded-lg object-cover object-center md:h-[480px]"
+      src={selected}
+      alt="" />
+  </div>
+  <div class="grid grid-cols-5 gap-4 md:grid-cols-12">
+    {#each imageList as image}
+        <div role="button"
+            tabindex="0"
+            on:click={(event) => selected = event.target.src}
+            on:keydown={(event) => console.log(event)}
+            aria-label="select image">
+          <img
+            src={image}
+            class="object-cover object-center h-20 max-w-full shadow-md border border-gray-300 rounded-lg cursor-pointer" alt="gallery" />
+        </div>
+    {/each}
+  </div>
 </div>
+
+
+
+
