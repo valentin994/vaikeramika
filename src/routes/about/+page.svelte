019acdb0cb7abee09e09c412a3154e10afc1819a
valentin994 <valentin.vareskic@gmail.com>
2024-04-04 11:24:13 +0200

    added reviews with animation

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,7 @@
 <script>
-  import { fade, fly } from "svelte/transition";
+  import { fade, fly } from "svelte/transition";	
+  import { tweened } from 'svelte/motion';
+  import { backOut } from 'svelte/easing';
   let reviews = [
       { "id": 1, "name": "Marija", "review": "Ivica je pouzdan, dosao je na vrijeme i sve odradio po dogovoru" },
       { "id": 2, "name": "Valentin", "review": "Imao sam izvanredno iskustvo s ovim keramičarom. Bio je iznimno profesionalan i pažljiv u svakom koraku procesa. Kvaliteta njegovog rada je izvanredna, a cijena je bila vrlo razumna. Toplo preporučujem ovog keramičara svakome tko traži kvalitetan rad po pristupačnoj cijeni." },
@@ -14,16 +16,33 @@
     num++;
 
   }
+	let width = 0
+	let tweenedWidth = tweened(0,{
+		duration: 500,
+		easing: backOut
+	})
+    /**
+	 * @type {number | undefined}
+	 */
+    let clear;
+	$: {
+		tweenedWidth.set(width)
+        clearInterval(clear)
+	    clear = setInterval(changeReviewNum, 5000)
+	}
 </script>
 <h1 class="p-4 text-xl text-center">Nudimo širok spektar usluga, od jednostavnog postavljanja podnih pločica do složenih zidnih obloga i dekoracija.</h1>
 <div class="p-4 px-8">
     <img class="border border-gray-300 shadow-md rounded-md" src="./about_image.avif" alt="White tile laying" />
-    {#key num}
-        <div in:fly={{ y: 200, duration: 2000}} out:fade class="">
-            <h1 class="text-center text-secondary pt-8 italic text-xl">{reviews[num]["review"]}</h1>
-            <p class="text-textcol text-md italic bold text-end">{reviews[num]["name"]}</p>
-        </div>
-    {/key}
+    <div class="flex">
+            <div class="relative inline-flex w-full">
+                {#key num}
+                    <h1 in:fly={{y: 50,duration: 500}} out:fly={{y:-50,duration: 500}}  bind:clientHeight={width} class="text-center text-secondary pt-8 italic text-xl absolute">"{reviews[num]["review"]}" <span class="text-textcol text-sm italic">{reviews[num]["name"]}</span></h1>
+                {/key}
+
+		    <div style:height="{$tweenedWidth}px" style="width: 50px; height: 20px"/>
+            </div>
+    </div>
 </div>
 <div class="grid grid-cols-1 md:grid-cols-3 md:gap-4 w-full md:grid-row-1 px-4 md:px-24">
     <div class="flex border border-gray-300 rounded-md shadow-md w-full p-4 py-8 my-4 justify-between items-center bg-gray-300 bg-opacity-25">
