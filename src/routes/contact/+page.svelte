c00bb20c8fcf56a56bbaf6604f16dea8b5dfe976
valentin <valentin.vareskic@gmail.com>
2024-06-17 10:43:11 +0200

    minor changes to contact page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,6 +1,13 @@
+<h1 class="text-center text-3xl font-bold text-textcol py-4">KONTAKTIRAJTE NAS</h1>
+<div class="flex flex-col justify-center py-2">
+    <a href="tel:+38598311585" class="text-3xl text-secondary text-center">+385 98 311 585</a>
+    <div class="flex justify-center py-4">
+        <img class="w-12 mx-2" alt="Whatsapp" src="./wapp.svg" />
+        <img class="w-12 mx-2" alt="Telephone" src="./tel.svg" />
+    </div>
+</div>
 <div class="flex justify-center">
-<div class="max-w-xs md:max-w-lg lg:max-w-xl w-full mt-8">
-  <h1 class="text-center text-2xl text-secondary p-8">Kontaktirajte nas</h1>
+<div class="max-w-full md:max-w-lg lg:max-w-xl w-full mt-4 pb-8">
   <form class="bg-gray-300 bg-opacity-25 border border-gray-200 shadow-md rounded-md px-8 pt-6 pb-8 mb-4">
     <div class="mb-4">
       <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
@@ -21,11 +28,4 @@
     </div>
   </form>
 </div>
-</div>
-<div class="flex flex-col justify-center p-4">
-    <div class="flex justify-center p-4">
-        <img class="w-12 mx-2" alt="Whatsapp" src="./wapp.svg" />
-        <img class="w-12 mx-2" alt="Telephone" src="./tel.svg" />
-    </div>
-    <a href="tel:+38598311585" class="text-md text-secondary text-center">+385 98 311 585</a>
 </div>
